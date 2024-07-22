82196cf32b3aa298cce8d5e8591ffc6704483fab
Christian Schou <christian.schou@hotmail.com>
2024-07-22 20:30:39 +0200

    Remove unused code

--- src/Domain/GlobalUsings.cs
+++ src/Domain/GlobalUsings.cs
@@ -1,6 +1,1 @@
-﻿global using VatChecker.Domain.Common;
-global using VatChecker.Domain.Entities;
-global using VatChecker.Domain.Enums;
-global using VatChecker.Domain.Events;
-global using VatChecker.Domain.Exceptions;
-global using VatChecker.Domain.ValueObjects;
+﻿
